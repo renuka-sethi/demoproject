7c8c232907d3ff3e885d9e1beca0432476bbb7a4
Renuka Sethi <renuka.sethi@smartlearning.com>
2018-03-05 12:58:57 +0530

    changed line to this is a sample code

--- Renuka.java
+++ Renuka.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
+      System.out.println("Hello, this is a sample code");
       System.out.println("Changes after First commit!");
    }
 }
